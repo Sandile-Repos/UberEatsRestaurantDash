5c34aa17a664a274afc3ea6dfb3543d6170674df
Sandile-Repos <sandile.developer@gmail.com>
2023-03-20 00:56:03 +0200

    add react-google-places-autocomplete

--- src/modules/Settings/index.js
+++ src/modules/Settings/index.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Input, Card, Button } from "antd";
+import GooglePlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-google-places-autocomplete";
 
 const Settings = () => {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [address, setAddress] = useState(null);
+  const [coordinates, setCoordinates] = useState(null);
+
+  const getAddressLatLng = async (address) => {
+    setAddress(address);
+    const geocodedByAddress = await geocodeByAddress(address.label);
+    // console.log("geo", geocodedByAddress[0]);
+    const latLng = await getLatLng(geocodedByAddress[0]);
+    setCoordinates(latLng);
+  };
+
   return (
     <Card title="Restaurant Details" style={{ margin: 20 }}>
       <Form layout="vertical" wrapperCol={{ span: 8 }}>
         <Form.Item label="Restaurant Details" required>
           <Input placeholder="Enter restaurant name here" />
         </Form.Item>
-        <Form.Item label="Restaurant Address" required></Form.Item>
+        <Form.Item label="Restaurant Address" required>
+          <GooglePlacesAutocomplete
+            apiKey={API_KEY}
+            selectProps={{ value: address, onChange: getAddressLatLng }}
+          />
+        </Form.Item>
         <Form.Item>
           <Button type="primary">Submit</Button>
         </Form.Item>
       </Form>
+      <span>
+        {coordinates?.lat} - {coordinates?.lng}
+      </span>
     </Card>
   );
 };
