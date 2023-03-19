68d90bb23dca4dd1a4e11fc8bcc5d0f7ab4a9fb3
Sandile-Repos <sandile.developer@gmail.com>
2023-03-19 17:33:12 +0200

    add layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import DetailedOrder from "./modules/DetailedOrder";
 import Orders from "./modules/Orders";
 import { Route, Routes } from "react-router-dom";
+import { Layout, Image } from "antd";
+
+const { Sider, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Orders />} />
-      <Route path="order/:id" element={<DetailedOrder />} />
-    </Routes>
+    <Layout>
+      <Sider style={{ height: "100vh", backgroundColor: "white" }}>
+        <Image
+          src="https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg"
+          preview={false}
+        />
+      </Sider>
+      <Layout>
+        <Content>
+          <Routes>
+            <Route path="" element={<Orders />} />
+            <Route path="order/:id" element={<DetailedOrder />} />
+          </Routes>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Uber Eats Restaurant Dashboard &copy; 2022
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 
